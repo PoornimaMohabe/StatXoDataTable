9ddc54d15c94415c2dd285d4564ef6a6e3025905
PoornimaMohabe <147687634+PoornimaMohabe@users.noreply.github.com>
2024-07-04 17:26:12 +0530

    checked backend

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+}, {
+    versionKey: false
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+
+
+const router = express.Router();
+
+// user registration
+router.post('/register', register);
+
+
+// user login
+router.post('/login', login);
+
+module.exports = router;
